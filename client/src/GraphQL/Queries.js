ded73ce45dbca894111c4eecacba64faaaba4704
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2023-01-18 10:38:36 +0000

    unit schema set up

--- client/src/GraphQL/Queries.js
+++ client/src/GraphQL/Queries.js
@@ -10,3 +10,18 @@ export const LOAD_USERS = gql`
     }
   }
 `;
+
+export const LOAD_UNITS = gql`
+  query getAllUnits {
+    getAllUnits {
+      id
+      unitName
+      moveSpeed
+      shootValue
+      fightValue
+      healthPoints
+      leadershipValue
+      pointValue
+    }
+  }
+`;
