7f1692d5fe0dcf9d6257b9d4a6a3d4d4cd464552
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2023-01-18 12:48:55 +0000

    mutation bug fixing for create unit

--- server/Schemas/index.js
+++ server/Schemas/index.js
@@ -48,26 +48,56 @@ const RootQuery = new GraphQLObjectType({
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    createUser: {
-      type: UserType,
+    createUnit: {
+      type: UnitType,
       args: {
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
+        unitName: { type: GraphQLString },
+        moveSpeed: { type: GraphQLInt },
+        shootValue: { type: GraphQLInt },
+        fightValue: { type: GraphQLInt },
+        healthPoints: { type: GraphQLInt },
+        leadershipValue: { type: GraphQLInt },
+        pointValue: { type: GraphQLInt },
       },
       resolve(parent, args) {
-        userData.push({
-          id: userData.length + 1,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          email: args.email,
-          password: args.password,
+        unitData.push({
+          id: unitData.length + 1,
+          unitName: args.unitName,
+          moveSpeed: args.moveSpeed,
+          shootValue: args.shootValue,
+          fightValue: args.fightValue,
+          healthPoints: args.healthPoints,
+          leadershipValue: args.leadershipValue,
+          pointValue: args.pointValue,
         });
         return args;
       },
     },
   },
 });
+// const Mutation = new GraphQLObjectType({
+//   name: "Mutation",
+//   fields: {
+//     createUser: {
+//       type: UserType,
+//       args: {
+//         firstName: { type: GraphQLString },
+//         lastName: { type: GraphQLString },
+//         email: { type: GraphQLString },
+//         password: { type: GraphQLString },
+//       },
+//       resolve(parent, args) {
+//         userData.push({
+//           id: userData.length + 1,
+//           firstName: args.firstName,
+//           lastName: args.lastName,
+//           email: args.email,
+//           password: args.password,
+//         });
+//         return args;
+//       },
+//     },
+//   },
+// });
 
 module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
