d0efa2909016b309a18f4ab469e0e12d1f9fea56
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2023-01-19 12:22:29 +0000

    removes commented out code

--- client/src/GraphQL/mutations.js
+++ client/src/GraphQL/mutations.js
@@ -1,23 +1,5 @@
 import { gql } from "@apollo/client";
 
-// export const CREATE_USER_MUTATION = gql`
-//   mutation createUser(
-//     $firstName: String!
-//     $lastName: String!
-//     $email: String!
-//     $password: String
-//   ) {
-//     createUser(
-//       firstName: $firstName
-//       lastName: $lastName
-//       email: $email
-//       password: $password
-//     ) {
-//       id
-//     }
-//   }
-// `;
-
 export const CREATE_UNIT_MUTATION = gql`
   mutation createUnit(
     $unitName: String!
