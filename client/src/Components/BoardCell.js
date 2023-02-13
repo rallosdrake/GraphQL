07dfffeed49f039a54b300d15ec429499a2a7885
James Barlow <98394470+rallosdrake@users.noreply.github.com>
2023-02-13 16:34:36 +0000

    adds on hover event and styles

--- client/src/Components/BoardCell.js
+++ client/src/Components/BoardCell.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function BoardCell({ index }) {
   console.log(index, "this is the index");
-  return <td className="board-col">{index}</td>;
+  return (
+    <td
+      className="board-col"
+      //   onMouseOver={() => {
+
+      //   }}
+    >
+      <div className="index-number">{index}</div>
+    </td>
+  );
 }
 
 export default BoardCell;
